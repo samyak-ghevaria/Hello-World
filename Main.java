0dd2087f22e5dc6a715140eae97b90c6590dd9ef
samyak-ghevaria <68885130+samyak-ghevaria@users.noreply.github.com>
2020-08-14 12:56:23 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    System.out.println("Hello world!");
+    System.out.println("My name is Samyak Ghevaria");
+    System.out.println("I am 16 years old");
+    System.out.println("I enjoy playing video games and watching movies");
+    System.out.println("I also like playing sports with friends");
+    System.out.println("I don't like staying at home all day.");
+  }
+}
